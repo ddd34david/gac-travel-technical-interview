589dc4e40b8359361ef2950edd4b853abe20119a
David Figueredo <dfigueredo@informacion.es>
2023-11-08 18:24:54 +0100

    Creado CRUD usuarios

---
+++ src/Controller/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[Route('/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'user_edit', methods: ['GET','POST'])]
+    public function edit(Request $request, User $user, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Si se ha cambiado la contraseña, se realiza el hash antes de modificar el usuario
+            if($form->get('password')->getData()){
+                $user->setPassword(
+                    $userPasswordHasherInterface->hashPassword(
+                            $user,
+                            $form->get('password')->getData()
+                        )
+                    );
+            }
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
